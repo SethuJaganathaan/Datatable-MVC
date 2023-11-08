6ef9964ede1ef75abb67917b496f7d7070e20f58
SethuJaganathaan <sethuj99@gmail.com>
2023-11-08 11:44:24 +0530

    Initial commit

---
+++ Datatable-MVC/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Datatable_MVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Datatable_MVC.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> GetSampleUsers()
+        {
+            var users = new List<User>
+            {
+                new User {Id = 1, Name= "Luffy", Details = "gfhtfg"},
+                new User {Id = 2, Name= "Zoro", Details = "2313213"},
+                new User {Id = 3, Name= "Sanji", Details = "gfhtefweewrgfg"}
+            };
+            return users;
+        }
+
+        public IActionResult GetUserData()
+        {
+            return View("UserData");
+        }
+
+        public IActionResult GetUserDetailsData()
+        {
+            List<User> usersDetails = GetSampleUsers();
+            var jsonData = new
+            {
+                data = usersDetails.Select(x => new
+                {
+                    id = x.Id,
+                    name = x.Name,
+                    details = x.Details
+                })
+            };
+
+            return Json(new {data = usersDetails});
+        }
+    }
+}
---
+++ Datatable-MVC/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Datatable_MVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Details { get; set; } 
+    }
+}
