7c7a7ab4f8b7cf20a9d760bbb8dfd0b7c68106e5
SethuJaganathaan <sethuj99@gmail.com>
2023-11-30 00:32:13 +0530

    Datatable-breakup

---
+++ Datatable-MVC/Controllers/DatatableBreakupController.cs
@@ -0,0 +1,47 @@
+﻿using Datatable_MVC.D_Entities;
+using Datatable_MVC.D_Entities.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Datatable_MVC.Controllers
+{
+    public class DatatableBreakupController : Controller
+    {
+        private readonly Datatable _dbContext;
+        public DatatableBreakupController(Datatable dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public ActionResult Index()
+        {
+            var departments = _dbContext.Departments.ToList();
+            return View(departments);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Department> GetDepartmentById(int id)
+        {
+            var department = _dbContext.Departments.Include(d => d.Employees).FirstOrDefault(d => d.DepartmentId == id);
+            if (department == null)
+            {
+                return NotFound();
+            }
+            return department;
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Employee>> GetEmployeesByDepartmentId(int departmentId)
+        {
+            try
+            {
+                var employees = _dbContext.Employees.Where(e => e.DepartmentId == departmentId).ToList();
+                return Json(employees);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ Datatable-MVC/D-Entities/Department.cs
@@ -0,0 +1,10 @@
+﻿namespace Datatable_MVC.D_Entities
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        public List<Employee> Employees { get; set; }
+    }
+}
